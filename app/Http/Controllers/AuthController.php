3054345cccfcbda9820cc7fbf140be9d0f545a9d
abbosbek7677 <abbosbekk48@gmail.com>
2023-04-10 21:05:13 +0500

    auth or favorites

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,14 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
-    public function login(){
-
+    public function login(LoginRequest $request)
+    {
+        $user = User::where('email' ,$request->email)->first();
+        if(!$user || !Hash::check($request->password, $user->password)){
+            throw ValidationException::withMessages([
+                'email' => ['the provider are incorrect']
+            ]);
+        }
+        return response()->json([
+            'token'=> $user->createToken($request->email)->plainTextToken
+        ]);
     }
     public function register(){
-        
+
     }
 }
